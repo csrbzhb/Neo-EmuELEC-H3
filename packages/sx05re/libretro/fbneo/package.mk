4c481636b7cdbffa07eed08c6a76cc5f8bdad43e
matthew <asakous@gmail.com>
2022-05-16 09:26:57 +0800

    fix libretro fbneo compile problem

--- packages/sx05re/libretro/fbneo/package.mk
+++ packages/sx05re/libretro/fbneo/package.mk
@@ -25,11 +25,9 @@ if [[ "$TARGET_FPU" =~ "neon" ]]; then
 	PKG_MAKE_OPTS_TARGET+=" HAVE_NEON=1"
 fi
 
-if [ "$DEVICE" == "OdroidGoAdvance" ] || [ "$DEVICE" == "GameForce" ]; then
 	if [ "$ARCH" == "arm" ]; then
-	PKG_MAKE_OPTS_TARGET+=" platform=classic_armv8_a35"
+	PKG_MAKE_OPTS_TARGET+=" platform=classic_armv7_a7 "
 	fi
-fi
 
 }
 
